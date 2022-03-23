9c956ebfb5b527984ca29e272bd8f8e5b93a3f0b
RobotMan2412 <julian@scheffers.net>
2022-03-23 10:46:00 +0100

    Import the bosch logo.

--- sponsors.c
+++ sponsors.c
@@ -4,23 +4,26 @@
 // Handy definition.
 #define DEFINE_SPONSOR(logo_id, the_text) {\
     .text     = the_text,\
-    .logo     = logo_id##_logo_png,\
-    .logo_len = logo_id##_logo_png_len\
+    .logo     = logo_id##_png,\
+    .logo_len = logo_id##_png_len\
 }
 
 // Temporary sponsor placeholder of.
 #include "images/temp_logo.c"
 
+// Bosch logo.
+#include "images/logo/bosch.c"
+
 // The array.
 sponsor_t sponsors_arr[] = {
     // Temporary sponsor placeholder of.
-    DEFINE_SPONSOR(temp, "Placeholder"),
+    DEFINE_SPONSOR(temp_logo, "Placeholder"),
     // Bosch (need logo image).
-    DEFINE_SPONSOR(temp, "Bosch:\nBNO-055\nMBE-680"),
+    DEFINE_SPONSOR(logo_bosch, "Bosch:\nBNO-055\nMBE-680"),
     // Espressif (need logo image).
-    DEFINE_SPONSOR(temp, "Espressif:\nESP32 MCU"),
+    DEFINE_SPONSOR(temp_logo, "Espressif:\nESP32 MCU"),
     // Lattice (need logo image).
-    DEFINE_SPONSOR(temp, "Lattice:\nICE40 UP5K"),
+    DEFINE_SPONSOR(temp_logo, "Lattice:\nICE40 UP5K"),
     // Raspberry pi (need logo image).
-    DEFINE_SPONSOR(temp, "Raspberry pi:\nRP2040 coprocessor"),
+    DEFINE_SPONSOR(temp_logo, "Raspberry pi:\nRP2040 coprocessor"),
 };
